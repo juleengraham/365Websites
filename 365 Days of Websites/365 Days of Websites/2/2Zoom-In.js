b4533e03f33360683b73df8d38e2bf7c2ebb4450
Juleen Graham <juleengrah@gmail.com>
2019-01-04 00:13:14 -0500

    Day 2
    Playing with Buttons and js. Poor job.

---
+++ 365 Days of Websites/365 Days of Websites/2/2Zoom-In.js
@@ -0,0 +1,10 @@
+
+function increaseImg() {
+	document.getElementById("loved").style.width="1600px";
+	document.getElementById("loved").style.height="400px";
+}
+
+function decreaseImg() {
+	document.getElementById("loved").style.width="20px";
+	document.getElementById("loved").style.height="5px";
+}
